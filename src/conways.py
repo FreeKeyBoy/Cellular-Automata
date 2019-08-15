e47f9a4334e95071e33896ef944ec4bb74014c71
Freekeyboy <jjhutton81@gmail.com>
2019-08-14 18:10:32 -0700

    Got the game running. Grid, state and keyboard controls programmed.

--- src/conways.py
+++ src/conways.py
@@ -1,78 +1,111 @@
-import pygame, random
- 
-# Define some colors and other constants
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (25, 25, 25)
-WIN_SIZE = 500
-
-# 1. Create a set of initial states with simple pattern (Ex. blinker)
-cur_states = [0] * 400
-cur_states[10] = 1
-cur_states[30] = 1
-cur_states[50] = 1
-next_states = []
+import pygame, sys, random, copy
+from pygame.locals import *
 
 pygame.init()
- 
-# Set the width and height of the screen [width, height]
-size = (WIN_SIZE, WIN_SIZE)
+BLOCK_SIZE = BLOCK_W, BLOCK_H = (8, 8)
+BLOCKS = BLOCKS_COLS, BLOCKS_ROWS = 60,40
+size = width, height = (BLOCK_W * BLOCKS[0], BLOCK_H * BLOCKS[1])
 screen = pygame.display.set_mode(size)
-
-# Add a title
 pygame.display.set_caption("Conway's Game of Life")
- 
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
+
+BLACK = (  0,   0,   0)
+RED= (200, 0, 0 )
+
+class Grid:
+    paused = False
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.cells = []
+        for _ in range(BLOCKS_COLS):
+            cell = [False] * BLOCKS_ROWS
+            self.cells.append(cell)
+        self.randomize()
+
+    def randomize(self):
+        for x in range(BLOCKS_COLS):
+            for y in range(BLOCKS_ROWS):
+                if random.choice([True, False]):
+                    self.cells[x][y] = True
+
+    def toggle_pause(self):
+        self.paused = not self.paused
+
+    def click_at(self, point):
+        if self.paused:
+            x, y = point
+            block_x, block_y = x // BLOCK_W, y // BLOCK_H
+            self.cells[block_x][block_y] = not self.cells[block_x][block_y]
+
+    def draw_grid(self):
+        BLUISH = (30, 30, 100)
+        for i in range(BLOCKS_COLS):
+            start_point = (i * BLOCK_W, 0)
+            end_point = (i * BLOCK_W, height)
+            pygame.draw.line(self.screen, BLUISH, start_point, end_point, 1)
+        for i in range(BLOCKS_ROWS):
+            start_point = (0, i * BLOCK_H)
+            end_point = (width, i * BLOCK_H)
+            pygame.draw.line(self.screen, BLUISH, start_point, end_point, 1)
+
+    def draw_cells(self):
+        for x in range(BLOCKS_COLS):
+            for y in range(BLOCKS_ROWS):
+                if self.cells[x][y]:
+                    x2, y2 = x * BLOCK_W, y * BLOCK_H
+                    cell = pygame.Rect([x2+1, y2+1, BLOCK_W-1, BLOCK_H-1])
+                    pygame.draw.rect(self.screen, RED, cell)
+
+    def border_at(self, point):
+        borders = 0
+        x, y = point
+        points = ((x+1, y), (x+1, y+1), (x, y+1), (x-1, y+1), (x-1, y),
+                  (x-1, y-1), (x, y-1), (x+1, y-1))
+        for x2, y2 in points:
+            try:
+                if 0 <= x2 < BLOCKS_COLS and 0 <= y2 < BLOCKS_ROWS:
+                    if self.cells[x2][y2]:
+                        borders += 1
+                else:
+                    continue
+            except IndexError:
+                pass
+        return borders
+
+    def step(self):
+        if self.paused:
+            return
+        cells = copy.deepcopy(self.cells)
+        for x in range(BLOCKS_COLS):
+            for y in range(BLOCKS_ROWS):
+                borders = self.border_at((x, y))
+                if not self.cells[x][y]:
+                    if borders == 3:
+                        cells[x][y] = True
+                elif self.cells[x][y]:
+                    if not 2 <= borders <= 3:
+                        cells[x][y] = False
+        self.cells = cells
+
+grid = Grid(screen)
 clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
- 
-    # --- Game logic should go here
-    
-    # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    # Any live cell with two or three live neighbours lives on to the next generation.
-    # Any live cell with more than three live neighbours dies, as if by overpopulation.
-    # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-    
-    # 3. Work on rules that i) look at all neighbors, ii) save new state
-    # in next_states[]
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_SPACE:
+                grid.toggle_pause()
+            elif event.key == K_r:
+                grid.randomize()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            grid.click_at(pygame.mouse.get_pos())
 
-    # --- Screen-clearing code goes here
- 
-    # Here, we clear the screen to gray. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    screen.fill(GRAY)
- 
-    # --- Drawing code should go here
-    cur_index = 0
-    x = 5
-    while x < 500: 
-        y = 5
-        while y < 500:
-            # 2. Draw based on values in cur_states
-            state = cur_states[cur_index]
-            # 4. Draw based on values in next_states
-            if state == 0:
-                pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, 20, 20) )
-            else:
-                pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, 20, 20) )
-            cur_index += 1
-            y += 25
-        x += 25
-        
-    # --- Go ahead and update the screen with what we've drawn.
+    screen.fill(BLACK)
+    grid.draw_grid()
+    grid.draw_cells()
+    clock.tick(120)
     pygame.display.flip()
- 
-    # --- Limit to 5 frames per second
-    clock.tick(5)
- 
-# Close the window and quit.
-pygame.quit()
+    grid.step()
